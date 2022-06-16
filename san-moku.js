ac7c49ff3b8ce3790846c71d1081ac1e92a9574a
ayapes <aya@ymok.me>
2022-06-16 14:48:54 +0900

    0616 14:48

--- san-moku.js
+++ san-moku.js
@@ -1,7 +1,22 @@
-const maru1 = document.getElementsById('item1');
-const maru = "あ";
+// const maru1 = document.getElementsById('item1');
+// const maru = "あ";
 // クリックイベントが起こったときに動作する部分
-maru1.addEventListener('click', function () {
-    maru1.textContent = maru;
-    maru1.setAttribute('class', 'maru');
-})
+// maru1.addEventListener('click', function () {
+//     maru1.textContent = maru;
+//     maru1.setAttribute('class', 'maru');
+// });
+
+// const maru2 = document.getElementById('item2');
+// const maru = "○";
+// maru2.addEventListener('click',function(){
+//     maru2.textContent = maru;
+//     maru2.setAttribute('class', 'maru');
+
+//             })
+
+for(i = 0; i < 9; i++){
+    document.getElementById('item' + i).addEventListener('click',function() {
+        this.textContent = '◯';
+        this.setAttribute('class', 'maru');
+    })
+}
