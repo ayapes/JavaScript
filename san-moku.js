06ad80bdceb2ad9be4381c6407047b4f828d1516
ayapes <web@ymok.me>
2022-06-20 16:05:56 +0900

    0620 16:05

--- san-moku.js
+++ san-moku.js
@@ -14,9 +14,44 @@
 
 //             })
 
-for(i = 0; i < 9; i++){
-    document.getElementById('item' + i).addEventListener('click',function() {
-        this.textContent = '◯';
-        this.setAttribute('class', 'maru');
-    })
+
+const win_patterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+let field = document.querySelectorAll(".field");
+let count = 0;
+
+for (let i = 0; i < field.length; i++) {
+    field[i].addEventListener('click', function () {
+        count++;
+        if (count % 2 == 1) {
+            this.textContent = "○";
+            this.setAttribute('class', 'maru');
+        } else {
+            this.textContent = "×";
+            this.setAttribute('class', 'batsu');
+        }
+
+    }
+    )
+}
+
+
+for (let p = 0; p < win_patterns.length; p++) {
+    console.log(p);
+    function () {
+        if (win_patterns[p].every((n) => n == "○")) {
+            field.style.backgroundColor = "red";
+        } else {
+            field.style.backgroundColor = "blue";
+        }
+    }
 }
