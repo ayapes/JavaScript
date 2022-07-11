a82dfa64c61662d54d683173f7d9b9807f7c370f
ayapes <web@ymok.me>
2022-07-11 15:20:41 +0900

    07-11 15:20

--- test02.js
+++ test02.js
@@ -1,5 +1,6 @@
 
 const aaa = document.querySelectorAll(".aaa");
+const line = document.querySelector("#line")
 const win_patterns = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,6 +13,7 @@ const win_patterns = [
 ];
 
 let count = 0;
+let flag = false;
 
 for (let i = 0; i < aaa.length; i++) {
     aaa[i].addEventListener('click', function () {
@@ -23,29 +25,39 @@ for (let i = 0; i < aaa.length; i++) {
             this.textContent = "☆";
             // this.setAttribute('class', 'batsu');
         }
-
+        hantei();
     }
-    )
-    for (let j = 0; j < win_patterns.length; j++) {
-        let bufs = [];
-        
-        for (let k = 0; k < win_patterns[j].length; k++) {
-             bufs.push(aaa[win_patterns[j][k]].textContent);
-        }
-        console.log("j" + j + bufs);
-        bool1 = bufs.every((n) => n == "★");
-        bool2 = bufs.every((n) => n == "☆");
-        if(bool1){
-            console.log("あってます！");
-    }else{
-        console.log("あってません！"); 
-    }
-    
+    ); 
 }
 
-
+function hantei(){
+for (let j = 0; j < win_patterns.length; j++) {
+    let bufs = [];
     
-}
+    for (let k = 0; k < win_patterns[j].length; k++) {
+         bufs.push(aaa[win_patterns[j][k]].textContent);
+         console.log("j" + j + bufs);
+         bool1 = bufs.every((n) => n == "☆");
+         bool2 = bufs.every((n) => n == "★");
+         if(bool1){
+             line.textContent= "先手の勝利！";
+             console.log("あってます！");
+         }if(bool2){
+         line.textContent ="後手の勝利！";
+         console.log("あってません！");
+         }
+    }
+
+}}
+
+function sum(val1,val2){
+    return val1 + val2;}
+
+let result = sum(10,20);
+console.log("足し算の結果は・・・" + result);
+
+
+
 // const win_patterns[i] = patterns;
 // for (let i = 0; i < 8; i++){
 //     // console.log(field[win_patterns[i][0]] == field[win_patterns[i][1]] == field[win_patterns[i][2]] == 1);
