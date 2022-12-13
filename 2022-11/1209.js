8da7ba4845d1f90821c0f7ddb3f9ce9a30624b37
ayapes <web@ymok.me>
2022-12-13 13:05:31 +0900

    2022-12-13

---
+++ 2022-11/1209.js
@@ -0,0 +1,10 @@
+const arry = [10,2,7,3,9];
+//昇順：小さい数値から大きい数値への並び替え
+function ascending( a,b ){
+    return a-b;
+}
+
+//降順；大きい数値から小さい数値への並び替え
+function descending( a,b ){
+    return b-a;
+}
---
+++ 2022-11/1213.js
@@ -0,0 +1,10 @@
+console.log("処理A");
+
+if( true ){
+    console.log("処理B");
+} else{
+    console.log("処理C");
+}
+
+console.log("処理D");
+
